19618ff16a219bb2f7189409d6a57cbde2cdb05c
raphac <raphac@users.noreply.github.com>
2018-04-30 16:54:12 +0200

    Add first version of nuget package creation powershell script

---
+++ PackDebug.ps1
@@ -0,0 +1,13 @@
+$configuration = "Debug"
+rm bin -r -fo -ErrorAction Ignore
+$timestamp = Get-Date -Format yyMMddHHmmss
+dotnet pack --configuration $configuration --include-symbols
+
+$path = ".\bin\$configuration\Symbols\"
+If(!(test-path $path))
+{
+      New-Item -ItemType Directory -Force -Path $path
+}
+
+get-childitem -path ".\bin\$configuration\" -filter "*.symbols.nupkg" | move-item -Destination ".\bin\$configuration\Symbols\"
+pause
